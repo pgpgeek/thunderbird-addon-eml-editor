2587a917b4e7ccffd6bb48bd895e3c9ecc9a5b5e
pgpgeek <dave-hill@dyrk.org>
2020-12-04 18:04:33 +0100

    Attachements is now saved in eml -- need adapt the reader to detect & display attachments

--- scripts/others/save_eml.js
+++ scripts/others/save_eml.js
@@ -1,40 +1,96 @@
-const EMLHeaderFile = 
+const EMLHeaderFileTemplate = 
 `X-Unsent: 1
 To: %to%
 Cc: %cc%
 Subject: %subject%
-Content-Type: text/html; charset=UTF-8
+Content-Type: multipart/mixed; boundary=--boundary_text_string
 MIME-Version: 1.0
 
-%body%`;
+----boundary_text_string
+Content-Type: text/html; charset=UTF-8
 
-function formatEmail(contents)
+%body%
+
+%attachments%
+
+----boundary_text_string--
+`;
+
+const EMLAttachements = 
+`
+----boundary_text_string
+Content-Type: application/octet-stream;;
+Content-Disposition: attachment;
+        filename="%filename%"
+Content-Transfer-Encoding: base64
+
+%binary_data%
+
+`;
+
+function formatEmail(contents, attachements)
 {
   let to      = contents.to .length >= 1  ? contents.to.join(', ')  : '',
       cc      = contents.cc .length >= 1  ? contents.cc.join(', ')  : '',
       subject = contents.to .length >= 1  ? contents.subject : '',
-      fileContents = EMLHeaderFile;
+      fileContents = EMLHeaderFileTemplate;
+  attachements = attachements ? attachements : "";
   fileContents  = fileContents.replace(/%to%/,      to);
   fileContents  = fileContents.replace(/%cc%/,      cc);
   fileContents  = fileContents.replace(/%subject%/, subject);
   fileContents  = fileContents.replace(/%body%/,    contents.body);
+
+  fileContents  = fileContents.replace(/%attachments%/,    attachements);
   return fileContents;
 }
 
-// https://stackoverflow.com/questions/54120017/is-there-a-way-to-add-attachments-to-an-eml-file-created-in-javascript-or-other
 
-x = new ComposeManager();
+function formatAttachements(mailerCommon)
+{
+  return new Promise(async (res, err) => {
+    files = await mailerCommon.getAttachments();
+    files_list = files.map(async (file) => {
+      let nfile = await file.getFile();
+          return await (async (nfile) => {
+            return new Promise((res2) => {
+              reader  = new FileReader();
+              reader.addEventListener("load", function () {
+                let result = reader.result,
+                    fileType = result ? result.match(/data:(.*?);base64/) : null;
+                res2({
+                    file: nfile,
+                    type : fileType ? fileType[1] : "text/html",
+                    contents: !result || result.indexOf(',') == -1 ? "error": result.split(',')[1]
+                  });
+              }, false);
+              reader.readAsDataURL(nfile);
+            });
+          })(nfile);
+      });
+      files = await Promise.all(files_list);
+      res(files.map( file => {
+        let filename     = file.file.name,
+            filetype     = file.type,
+            binary_data  = file.contents,
+            fileContents = EMLAttachements;
 
-// New Feature - Attachments
-x.getAttachments().then(files => files.map( file =>
-  file.getFile().then( ((fileData, nfile) => {
-    var reader  = new FileReader();
-    reader.addEventListener("load", function () {
-      console.log(fileData, reader.result);
-    }, false);
-    reader.readAsBinaryString(nfile);
-  }).bind(null, file))
-));
+        fileContents  = fileContents.replace(/%filename%/,     filename);
+        fileContents  = fileContents.replace(/%filetype%/,     filetype);
+        fileContents  = fileContents.replace(/%binary_data%/,  binary_data);
+        return fileContents;
+      }).join("\n"));
+    });
+}
 
 
-x.getTextCompose().then(email => x.saveEMLFile(formatEmail(email)))
+
+mailerCommon = new ComposeManager();
+mailerCommon.getTextCompose().then(async (email) => {
+  let message = null,
+      attachements = null;
+  attachements = await formatAttachements(mailerCommon);
+  message = message = formatEmail(email, attachements);
+  console.log(attachements);
+  mailerCommon.saveEMLFile(message);
+
+});
