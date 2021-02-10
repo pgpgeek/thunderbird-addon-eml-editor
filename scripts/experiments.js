84bdd063a894504da95764d9d6a5cfa38104205a
jorgk3 <jorgk@jorgk.com>
2021-02-10 03:35:57 +0100

    Replace homegrown (mini-)MIME parsing with WE Experiment.

--- scripts/experiments.js
+++ scripts/experiments.js
@@ -1,6 +1,7 @@
 /* globals ExtensionCommon */
 
 var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+var { MailServices } = ChromeUtils.import("resource:///modules/MailServices.jsm");
 
 // Implements the functions defined in the experiments section of schema.json.
 var EmlEditor = class extends ExtensionCommon.ExtensionAPI {
@@ -14,6 +15,32 @@ var EmlEditor = class extends ExtensionCommon.ExtensionAPI {
         async getPrio() {
           let win = Services.wm.getMostRecentWindow("msgcompose");
           return win ? win.gMsgCompose.compFields.priority : null;
+        },
+        openEML() {
+          let win = Services.wm.getMostRecentWindow("mail:3pane");
+          if (!win) return;
+          let fp = Cc["@mozilla.org/filepicker;1"].createInstance(Ci.nsIFilePicker);
+          fp.init(
+            win,
+            "EML File",
+            Ci.nsIFilePicker.modeOpen
+          );
+
+          fp.appendFilter("EML", "*.eml");
+
+          fp.open(rv => {
+            if (rv != Ci.nsIFilePicker.returnOK) {
+              return;
+            }
+            let msgWindow = Cc["@mozilla.org/messenger/msgwindow;1"]
+                              .createInstance(Ci.nsIMsgWindow);
+
+            // Parameters need more work here to select correct identity/From.
+            let fileURL = fp.fileURL.mutate().setQuery("type=application/x-message-display").finalize();
+            MailServices.compose.OpenComposeWindow(null, {}, fileURL.spec,
+              Ci.nsIMsgCompType.Draft,
+              Ci.nsIMsgCompFormat.Default, null, null, msgWindow);
+          });
         },
       },
     };
